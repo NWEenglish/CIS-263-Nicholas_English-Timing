3cef7972bcb5f0ea482b944a40d17f23ad06045d
Nicholas English <englisni@arch07.cis.gvsu.edu>
2018-04-02 15:33:51 -0400

    Finished file.

---
+++ include/Celebrity.h
@@ -0,0 +1,63 @@
+/**********************************************************************
+ * This is the .h file used by the Celebrity.cpp file, which will
+ * implement the functions from this file.
+ *
+ * @author Nicholas English
+ * @version 1.0
+ * @date 4/4/2018
+ *********************************************************************/
+
+/* Header gaurds */
+#ifndef __H_celeb__
+#define __H_celeb__
+
+/* Libraries */
+#include <stirng>
+#include <iostream>
+
+class Celebrity {
+
+    public:
+
+	/* Contructor */
+	Celebrity();
+
+	/* Mutators */
+	// Set name
+	void setName(std::string n);
+
+	// Set score
+	void setScore(int s);
+
+	// Set areaOfWork
+	void setAreaOfWork(std::string a);
+
+	// Set ifMet
+	void setIfMet(bool m);
+
+	/* Accessors */
+	// Get name
+	std::string getName() const;
+
+	// Get score
+	int getScore() const;
+
+	// Get areaOfWork
+	std::string getAreaOfWork() const;
+
+	// Get ifMet
+	bool getIfMet() const;
+
+    /*****************************************************************/
+
+    private:
+
+	/* Variabes used by the celebrity object */
+	std::string name;
+	int score;
+	std::string areaOfWork;
+	bool ifMet;
+
+};
+
+#endif // __H_celeb__
